72a19db826e3e81a20ff88d6b7ae33fc4bbd2c01
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-11-04 19:57:18 +0530

    Updates

---
+++ precache-manifest.f6134d2cd792e09428c09db4943b06ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2432e91dbdbba7a3c68070fd0644bc9f",
+    "url": "/shivamjs/index.html"
+  },
+  {
+    "revision": "9d6c600a9a18c53746f7",
+    "url": "/shivamjs/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/shivamjs/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/shivamjs/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6c600a9a18c53746f7",
+    "url": "/shivamjs/static/js/main.35d010d7.chunk.js"
+  },
+  {
+    "revision": "0906a3df792ab29c581a",
+    "url": "/shivamjs/static/js/runtime-main.bd8d10d4.js"
+  }
+]);
